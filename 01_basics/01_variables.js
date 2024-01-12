6b8ef3dd8354e0b82e8d0e07afa61e4dbd25dd12
Abhigyan-Thakur <129932686+Abhigyan-Thakur@users.noreply.github.com>
2024-01-12 09:57:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 19090
+let accountEmail= 'abhoigybjsdh2783.com'
+var accountPassword="12345678"
+accountCity="jaipur"
+// accountId=90887
+/*prefer not to use Var
+because of issue in block scope and funtional scope*/
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
