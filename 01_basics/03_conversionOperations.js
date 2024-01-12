9e3d8921b78637f121ee980b969ba01946397537
Abhigyan-Thakur <129932686+Abhigyan-Thakur@users.noreply.github.com>
2024-01-12 11:22:53 +0000

    conversionOperators

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,16 @@
+let score=null
+console.log( typeof score);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber); 
+
+// "33"=>number 33
+// null=>number 0
+let vNumber=1
+console.log(typeof(vNumber));
+let convToBool=Boolean(vNumber);
+console.log(typeof(convToBool));
+console.log(convToBool);
+
+// 1=> bollean true
