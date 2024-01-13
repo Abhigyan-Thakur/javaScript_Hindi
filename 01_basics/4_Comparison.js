6500064333e391c371d6c4e504b77d56eed875f2
Abhigyan-Thakur <129932686+Abhigyan-Thakur@users.noreply.github.com>
2024-01-13 06:49:22 +0000

    comparison

--- 01_basics/4_Comparison.js
+++ 01_basics/4_Comparison.js
@@ -4,3 +4,17 @@
 console.log("2">1)
 console.log("02">1)
 console.log(true!==false)
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined>0)
+console.log(undefined<0);
+
+
+// StrictCheck
+console.log(2==="2");
+console.log(2===2)
