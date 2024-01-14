f378387b402ff822a43f81c97ad1d6c00b4f4405
Abhigyan Thakur <abhigyanthakur2710@gmail.com>
2024-01-14 10:52:36 +0530

    String Methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const name = "aman"
+const val = 45
+console.log(`Hello my name is ${name} and token number is ${val}`);
+console.log(name + val);
+
+
+const gameOn = new String("    aman_raj_heree____   ");
+// substring, slice,trim go to documentation,indexof,charAt,toUppercase,length,__proto__,replace,includes(to ask if data resides)
+
+console.log(gameOn.toUpperCase());
+console.log(gameOn.charAt(4));
+console.log(gameOn.indexOf('r'));
+console.log(gameOn.length);
+console.log(gameOn.trim())
+console.log(gameOn.replace('raj', 'Thakur'));
+console.log(gameOn.includes('raj'))
+console.log(gameOn.slice(2, 6))
+console.log(gameOn.substring(6))
