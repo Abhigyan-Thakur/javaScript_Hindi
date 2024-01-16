202b54e595579624d9a0a67eb520fa94f0241801
Abhigyan Thakur <abhigyanthakur2710@gmail.com>
2024-01-16 21:13:32 +0530

    here learnt about iffi its purpose

---
+++ 03_basics/04_iffi.js
@@ -0,0 +1,9 @@
+// iffi: iffi is used to meet two purpose 1>iffi is used to remove the pollution(variables) of global scope 
+// 2>iffi is used for fast function execution
+(function chai() {
+    console.log("DB Connected");
+})();//here semicolon is mandatory as it works as the end of the 1st iffi function
+
+function secondChai() {
+    console.log("Second chai");
+}
