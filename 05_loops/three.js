62236c3ffb8e509a5b12a08d48184c71053a6849
Abhigyan Thakur <abhigyanthakur2710@gmail.com>
2024-01-19 07:58:24 +0530

    three_loops

---
+++ 05_loops/three.js
@@ -0,0 +1,46 @@
+// assign an array with some values
+// now retreive the val using for of loop (Note-it works on arrays,string and object also)
+// create a map object, assig some values using map.set() , and use forof loop,simply console.log(map) to retreive data
+
+const A = [1, 2, 3, 45, 46];
+for (const iterator of A) {
+    // console.log(iterator);
+}
+const mapObj = new Map();
+mapObj.set(1, "mob");
+mapObj.set(2, "mob1");
+mapObj.set(3, "mob2");
+console.log(mapObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// create a file four.js
+// create an object having both keys and values
+// use forin loop to retreive both the value of key and value
+
+// then also try to retreive values from array using forin
+
+
+// again create a map object assign some values and try to retreive data using forin loop
+
+// create five.js file
+// create an array and assign some values
+// try to retreive values from array using forEach using call-back functions(normal and arrow functions both)
+// try to print item, index,arr using forEach loop
+
+
